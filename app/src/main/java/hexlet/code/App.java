bc2ccacfa60815241e41af8ed090d82d4c0428dc
Vadim <ivlev_vm@n-vartovsk.ru>
2024-04-25 15:27:35 +0500

    GCD

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
+import hexlet.code.games.Gcd;
 
 import java.util.Scanner;
 
@@ -13,6 +14,7 @@ public class App {
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
         int choice = scanner.nextInt();
@@ -26,6 +28,9 @@ public class App {
                 break;
             case 3:
                 game = new Calc();
+                break;
+            case 4:
+                game = new Gcd();
                 break;
             default: System.exit(0);
         }
---
+++ app/src/main/java/hexlet/code/games/Gcd.java
@@ -0,0 +1,51 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+public class Gcd extends Engine {
+    protected String rules = "Find the greatest common divisor of given numbers.\n";
+
+    public Gcd() {
+        greeting();
+    }
+
+    @Override
+    protected void play() {
+        int countCorrectAnswer = 0;
+
+        while (true) {
+            int firstNum = generateRandomNum(startRange, endRange);
+            int secondNum = generateRandomNum(startRange, endRange);
+            int result = calculateGcd(firstNum, secondNum);
+            System.out.printf(questionMessage, getStringView(firstNum, secondNum));
+            System.out.printf(answerMessage);
+            int playerAnswer = scanner.nextInt();
+            if (playerAnswer == result) {
+                countCorrectAnswer++;
+                System.out.printf(correctMessage);
+                if (countCorrectAnswer == countCorrectAnswers) {
+                    System.out.printf(successMessage, playerName);
+                    break;
+                }
+            } else {
+                System.out.printf(error, playerAnswer, result, playerName);
+                break;
+            }
+        }
+    }
+
+    private String getStringView(int firstNum, int secondNum) {
+        return String.format("%d %d", firstNum, secondNum);
+    }
+
+    private int calculateGcd(int firstNum, int secondNum) {
+        while (firstNum != 0 && secondNum != 0) {
+            if (firstNum > secondNum) {
+                firstNum = firstNum % secondNum;
+            } else {
+                secondNum = secondNum % firstNum;
+            }
+        }
+        return firstNum + secondNum;
+    }
+}
