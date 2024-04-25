d2a3ddfd55f94e98c7e23a3d73f38acc9588a348
Vadim <ivlev_vm@n-vartovsk.ru>
2024-04-25 16:51:10 +0500

    Corrections linter errors

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -6,6 +6,6 @@ public class Cli {
     private static final Scanner SCANNER = new Scanner(System.in);
     public static void greeting() {
         System.out.print("Welcome to the Brain Games!\nMay I have your name? ");
-        System.out.println("Hello, " + SCANNER.nextLine());
+        System.out.println("Hello, " + SCANNER.nextLine() + "!");
     }
 }
